e2dcadf2dfcb9ec96bc91707d5c8e0d644fad884
StarPhantom1 <erick.nb09@gmail.com>
2026-06-07 19:12:16 -0300

    Update and rename Adivinhe o numero.py to Adivinhe o numero V 2.py

--- Adivinhe o numero V 2.py
+++ Adivinhe o numero V 2.py
@@ -3,24 +3,26 @@ import random
 numero = random.randint(1,10)
 nt = str(numero)
 acertou = False
+T = 0
 
 #Repetidor
 while not acertou:
+
     Ad = input("Adivinhe o numero:")
+    T +=1
     Adn = int(Ad)
         # Codigo após adivinhação
     if Adn == numero:
         acertou = True
         print ("O numero q eu escolhi era "+nt+ " você acertou!!!" )
+        print ("Você precisou de "+str(T)+" tentativas")
     else:
+        
         print ("Você errou :(")
         if int (Adn) >numero: print ("O numero é menor que "+ Ad +" tente novamente" )
         else: 
             print ("O numero é maior que "+ Ad +" tente novamente" )  
     
     
-
-
-
 
 
